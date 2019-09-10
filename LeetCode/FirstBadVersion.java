fd0b37f9c7efb3f7dcd5c3d65283a2d62231dea1
Uditi <uditiarora@gmail.com>
2019-09-10 22:09:47 +0530

    20

---
+++ LeetCode/FirstBadVersion.java
@@ -0,0 +1,22 @@
+/* The isBadVersion API is defined in the parent class VersionControl.
+      boolean isBadVersion(int version); */
+
+public class Solution extends VersionControl {
+    
+    
+    public int firstBadVersion(int n) {
+        int l = 1;
+        int r = n;
+        int mid = (l+r)/2;
+        while(l<r){
+            mid = l + (r-l)/2;
+            if(isBadVersion(mid)){
+                r = mid;
+            }
+            else{
+                l = mid+1;
+            }
+        }
+        return l;
+    }
+}
