b7aa92f27a32628eba45b9798373b5c5019cc7d2
Uditi <uditiarora@gmail.com>
2019-10-02 10:30:02 +0530

    36

---
+++ LeetCode/ContainsDuplicate-III.java
@@ -0,0 +1,16 @@
+class Solution {
+    public boolean containsNearbyAlmostDuplicate(int[] nums, int k, int t) {
+       
+         for(int i = 0;i<nums.length;i++)
+        {
+            for(int j = i+1;j<=i+k && j < nums.length;j++)
+            {
+             if(Math.abs((long)nums[i]-nums[j]) <= t)
+                return true;
+            }    
+        }
+        
+        return false;
+ 
+    }
+}
