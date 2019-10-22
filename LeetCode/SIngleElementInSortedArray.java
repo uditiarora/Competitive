faffaff3ea7be6944ac8ef7209fe1bc0e16fc7ec
Uditi <uditiarora@gmail.com>
2019-10-22 17:39:04 +0530

    44

---
+++ LeetCode/SIngleElementInSortedArray.java
@@ -0,0 +1,26 @@
+class Solution {
+    public int singleNonDuplicate(int[] nums) {
+        int low = 0, high = nums.length-1, mid;
+        int n = nums.length;
+        while(low<high){
+            mid = low + (high-low)/2;
+            if(mid%2 == 0){
+                if(mid<n-1 && nums[mid]==nums[mid+1]){
+                    low = mid+1;
+                }
+                else{
+                    high = mid;
+                }
+            }
+            else{
+                if(mid>0 && nums[mid] == nums[mid-1]){
+                    low = mid+1;
+                }
+                else{
+                    high = mid;
+                }
+            }
+        }
+        return nums[high];
+    }
+}
