c0810eea077973f790d03b2f74155255ba30e8c0
Uditi <uditiarora@gmail.com>
2019-11-12 05:42:01 +0530

    92

---
+++ LeetCode/NumberOfSquarefulArrays.java
@@ -0,0 +1,40 @@
+class Solution {
+    Map<Integer, Integer> cnt = new HashMap<>();
+    Map<Integer, Set<Integer>> graph = new HashMap<>();
+
+    public int numSquarefulPerms(int[] A) {
+        for (int x : A) {
+            cnt.put(x, cnt.getOrDefault(x, 0) + 1);
+            graph.putIfAbsent(x, new HashSet<>());
+        }
+
+        for (int x : cnt.keySet()) {
+            for (int y : cnt.keySet()) {
+                double c = Math.sqrt(x + y);
+                if (c == Math.floor(c)) {
+                    graph.get(x).add(y);
+                    graph.get(y).add(x);
+                }
+            }
+        }
+        int ans = 0;
+        for (int x :
+                cnt.keySet()) {
+            ans += dfs(x, A.length - 1);
+        }
+        return ans;
+    }
+
+    private int dfs(int node, int left) {
+        cnt.put(node, cnt.get(node) - 1);
+        int count = left == 0 ? 1 : 0;
+        for (int nei :
+                graph.get(node)) {
+            if (cnt.get(nei) != 0) {
+                count += dfs(nei, left - 1);
+            }
+        }
+        cnt.put(node, cnt.get(node) + 1);
+        return count;
+    }
+}
