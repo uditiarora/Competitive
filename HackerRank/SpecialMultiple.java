62c18f9c3249a1d2699dc77428fe27cf08b802ae
Uditi <uditiarora@gmail.com>
2019-11-13 09:52:08 +0530

    108

---
+++ HackerRank/SpecialMultiple.java
@@ -0,0 +1,48 @@
+import java.io.*;
+import java.math.*;
+import java.text.*;
+import java.util.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    // Complete the solve function below.
+    static String solve(int n) {
+        Queue<String> q = new LinkedList<String>();
+        q.add("9");
+        while(!q.isEmpty()){
+            String t = q.poll();
+
+            long temp = Long.parseLong(t);
+            if(temp%n == 0){
+                return t;
+            }
+            q.add(t+"0");
+            q.add(t+"9");
+        }
+        return "";
+    }
+
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) throws IOException {
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        int t = scanner.nextInt();
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        for (int tItr = 0; tItr < t; tItr++) {
+            int n = scanner.nextInt();
+            scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+            String result = solve(n);
+
+            bufferedWriter.write(result);
+            bufferedWriter.newLine();
+        }
+
+        bufferedWriter.close();
+
+        scanner.close();
+    }
+}
